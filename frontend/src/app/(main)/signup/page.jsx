9a8ef9f5683fae4798ae192ef2d6f242c829661e
Varsha Gupta <vrsa8896@gmail.com>
2024-03-25 23:58:24 +0530

    nj

--- frontend/src/app/(main)/signup/page.jsx
+++ frontend/src/app/(main)/signup/page.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
-  )
-}
+    <>
+      <div
+        className="flex items-center justify-center "
+        style={{
+          backgroundSize: "cover",
+          height: "100vh",
+          backgroundImage: `url("https://img.freepik.com/free-vector/blue-background-with-white-line-middle_483537-4472.jpg?w=1800&t=st=1711032251~exp=1711032851~hmac=46bb16dd2365fef83ddd64316554516f520511caa8c412ef2e301a1b1abaf8d5")`,
+        }}
+      >
 
-export default Signup
+        <div className="flex w-96 border rounded-xl ">
+        <div 
+          className="  w-96  bg-white p-5"
+          style={{}}
+        >
+          <h3>Hello,</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+            voluptatem corrupti accusantium placeat a enim, eius quod deserunt
+            reiciendis tempore magni reprehenderit fugiat mollitia vitae
+            necessitatibus veniam id voluptates veritatis.
+          </p>
+        </div>
+        <div
+          className="  w-96  bg-white p-5 rounded-xl  "
+          style={{}}
+        >
+          <h3>Hello,</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+            voluptatem corrupti accusantium placeat a enim, eius quod deserunt
+            reiciendis tempore magni reprehenderit fugiat mollitia vitae
+            necessitatibus veniam id voluptates veritatis.
+          </p>
+        </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Signup;
