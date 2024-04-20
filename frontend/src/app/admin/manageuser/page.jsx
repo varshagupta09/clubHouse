86ee6641848ba2084dd6dd4586e79f71aa65bcee
Rohit271298 <rohitsharma.9169@gmail.com>
2024-04-20 12:23:44 +0530

    ..

--- frontend/src/app/admin/manageuser/page.jsx
+++ frontend/src/app/admin/manageuser/page.jsx
@@ -2,83 +2,137 @@ import React from 'react'
 
 const ManageUser = () => {
   return (
-    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-  <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-    <thead className="text-xs text-gray-700 uppercase dark:text-gray-400">
-      <tr>
-        <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-          Product name
-        </th>
-        <th scope="col" className="px-6 py-3">
-          Color
-        </th>
-        <th scope="col" className="px-6 py-3 bg-gray-50 dark:bg-gray-800">
-          Category
-        </th>
-        <th scope="col" className="px-6 py-3">
-          Price
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr className="border-b border-gray-200 dark:border-gray-700">
-        <th
-          scope="row"
-          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
-        >
-          Apple MacBook Pro 17"
-        </th>
-        <td className="px-6 py-4">Silver</td>
-        <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">Laptop</td>
-        <td className="px-6 py-4">$2999</td>
-      </tr>
-      <tr className="border-b border-gray-200 dark:border-gray-700">
-        <th
-          scope="row"
-          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
-        >
-          Microsoft Surface Pro
-        </th>
-        <td className="px-6 py-4">White</td>
-        <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">Laptop PC</td>
-        <td className="px-6 py-4">$1999</td>
-      </tr>
-      <tr className="border-b border-gray-200 dark:border-gray-700">
-        <th
-          scope="row"
-          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
-        >
-          Magic Mouse 2
-        </th>
-        <td className="px-6 py-4">Black</td>
-        <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">Accessories</td>
-        <td className="px-6 py-4">$99</td>
-      </tr>
-      <tr className="border-b border-gray-200 dark:border-gray-700">
-        <th
-          scope="row"
-          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
-        >
-          Google Pixel Phone
-        </th>
-        <td className="px-6 py-4">Gray</td>
-        <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">Phone</td>
-        <td className="px-6 py-4">$799</td>
-      </tr>
-      <tr>
-        <th
-          scope="row"
-          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800"
-        >
-          Apple Watch 5
-        </th>
-        <td className="px-6 py-4">Red</td>
-        <td className="px-6 py-4 bg-gray-50 dark:bg-gray-800">Wearables</td>
-        <td className="px-6 py-4">$999</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+    <>
+  {/* Component: Simple Table */}
+  <div className="w-full overflow-x-auto">
+    <table
+      className="w-full text-left border border-collapse rounded sm:border-separate border-slate-200"
+      cellSpacing={0}
+    >
+      <tbody>
+        <tr>
+          <th
+            scope="col"
+            className="h-12 px-6 text-sm font-medium border-l first:border-l-0 stroke-slate-700 text-slate-700 bg-slate-100"
+          >
+            Name
+          </th>
+          <th
+            scope="col"
+            className="h-12 px-6 text-sm font-medium border-l first:border-l-0 stroke-slate-700 text-slate-700 bg-slate-100"
+          >
+            Title
+          </th>
+          <th
+            scope="col"
+            className="h-12 px-6 text-sm font-medium border-l first:border-l-0 stroke-slate-700 text-slate-700 bg-slate-100"
+          >
+            Company
+          </th>
+          <th
+            scope="col"
+            className="h-12 px-6 text-sm font-medium border-l first:border-l-0 stroke-slate-700 text-slate-700 bg-slate-100"
+          >
+            Role
+          </th>
+          <th
+            scope="col"
+            className="h-12 px-6 text-sm font-medium border-l first:border-l-0 stroke-slate-700 text-slate-700 bg-slate-100"
+          >
+            Username
+          </th>
+        </tr>
+        <tr>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Ayub Salas
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Designer
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Carroll Group
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Member
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            salas_a
+          </td>
+        </tr>
+        <tr>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Agnes Sherman
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Developer
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Apple
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Admin
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            shermanagnes
+          </td>
+        </tr>
+        <tr>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Jemma Cummings
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Senior Designer
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            20goto10
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Member
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            jemmaC
+          </td>
+        </tr>
+        <tr>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Jimi Cardenas
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Copywriter
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Wind-UI
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Owner
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            cardenasji
+          </td>
+        </tr>
+        <tr>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Mateusz Tucker
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Project Manager
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Tailwindui
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            Member
+          </td>
+          <td className="h-12 px-6 text-sm transition duration-300 border-t border-l first:border-l-0 border-slate-200 stroke-slate-500 text-slate-500 ">
+            mt
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  {/* End Simple Table */}
+</>
+
 
   )
 }
