0b15619134e70442f52acfbcfac91e972c041192
Rohit271298 <rohitsharma.9169@gmail.com>
2024-04-20 11:32:42 +0530

    ..

--- frontend/src/app/admin/navbar.jsx
+++ frontend/src/app/admin/navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AdminNavbar = () => {
   return (
-    <div>AdminNavbar</div>
+    <div></div>
   )
 }
 
---
+++ frontend/src/app/template.jsx
@@ -0,0 +1,16 @@
+'use client';
+import React,{useEffect} from 'react';
+import Navbar from "@/components/navbar";
+import Footer from "@/components/footer";
+
+const template = ({children}) => {
+  return (
+    <div>
+        <Navbar/>
+        {children}
+        <Footer/>
+    </div>
+  )
+}
+
+export default template
