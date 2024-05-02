d90200c16322c8e84bdbde6c6cbde6234baa2b13
Rohit271298 <rohitsharma.9169@gmail.com>
2024-05-02 20:02:48 +0530

    ..

--- frontend/src/app/(main)/contact/page.jsx
+++ frontend/src/app/(main)/contact/page.jsx
@@ -6,7 +6,7 @@ const ContactUsPage = () => {
   return (
     <section style={{ background: "#CADCFC" }}>
     <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-      <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+      <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-black">
         Contact Us
       </h2>
      
@@ -14,14 +14,14 @@ const ContactUsPage = () => {
         <div>
           <label
             htmlFor="email"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-800"
           >
             Your email
           </label>
           <input
             type="email"
             id="email"
-            className="shadow-sm bg-blue-50 border border-blue-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5   dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+            className="shadow-sm bg-blue-50 border border-blue-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5   dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
             placeholder="name@reactuihub.com"
             required=""
           />
@@ -29,14 +29,14 @@ const ContactUsPage = () => {
         <div>
           <label
             htmlFor="subject"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-800"
           >
             Subject
           </label>
           <input
             type="text"
             id="subject"
-            className="block p-3 w-full text-sm text-gray-900 bg-blue-50 rounded-lg border border-blue-300 shadow-sm focus:ring-primary-500 focus:border-primary-500  dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+            className="block p-3 w-full text-sm text-gray-900 bg-blue-50 rounded-lg border border-blue-300 shadow-sm focus:ring-primary-500 focus:border-primary-500  dark:placeholder-gray-400 dark:text-gray-900 dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
             placeholder="Let us know how we can help you"
             required=""
           />
@@ -44,14 +44,14 @@ const ContactUsPage = () => {
         <div className="sm:col-span-2">
           <label
             htmlFor="message"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-800"
           >
             Your message
           </label>
           <textarea
             id="message"
             rows={6}
-            className="block p-2.5 w-full text-sm text-gray-900 bg-blue-50 rounded-lg shadow-sm border border-blue-300 focus:ring-primary-500 focus:border-blue-500 dark:placeholder-gray-400 dark:text-white  "
+            className="block p-2.5 w-full text-sm text-gray-900 bg-blue-50 rounded-lg shadow-sm border border-blue-300 focus:ring-primary-500 focus:border-blue-500 dark:placeholder-gray-400 dark:text-gray-900  "
             placeholder="Leave a comment..."
             defaultValue={""}
           />
