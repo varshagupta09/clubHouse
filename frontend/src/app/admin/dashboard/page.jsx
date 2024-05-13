1bf2d06e551dea5c7efd8e253ce1dfb5cfa912a2
Varsha Gupta <vrsa8896@gmail.com>
2024-05-13 23:32:28 +0530

    ..

--- frontend/src/app/admin/dashboard/page.jsx
+++ frontend/src/app/admin/dashboard/page.jsx
@@ -1,20 +1,12 @@
-'use client';
+"use client";
 import Link from "next/link";
 import React, { useEffect } from "react";
 
-
 const Dashboard = () => {
-
-   
-    
   return (
     <div>
-
-
       <div className=" w-full ">
         <div style={{ display: "flex", width: "100%" }}>
-       
-
           <div className="p-4 w-full h-screen bg-gray-800">
             <div className="p-4 text-2xl font-semibold text-white  ">
               <h1>Template Dashboard</h1>
@@ -132,53 +124,54 @@ const Dashboard = () => {
                   />
                 </label>
 
-                <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
+                <div className="flex items-center justify-center rounded  h-28 bg-gray-800">
                   <p className="text-2xl text-gray-400 "></p>
                 </div>
               </div>
             </div>
 
-            <div className=" grid p-5 border-2 text-gray-600 rounded border-gray-200 bg-gray-100 m-3 h-3/5 ">
-
-                <div>
-              <div className="flex  h-auto justify-between">
-                <div>
-                  <h5 className=" text-xl font-bold tracking-tight ">
-                    Total new visitor
-                  </h5>
+            <div className=" grid p-5 border-2 text-gray-600 rounded border-gray-200 bg-white m-3 h-full ">
+              <div>
+                <div className="flex  h-auto justify-between">
+                  <div>
+                    <h5 className=" text-xl font-bold tracking-tight ">
+                      Total new visitor
+                    </h5>
+                  </div>
+                  <div className="">
+                    <button className="mx-4 text-white bg-gray-400 border-gray-500 rounded-md hover:bg-white  hover:text-black px-2">
+                      Day
+                    </button>
+                    <button className="mx-4 text-white bg-gray-400 border-gray-500 rounded-md hover:bg-white  hover:text-black  px-2">
+                      Week
+                    </button>
+                    <button className="mx-4 text-white bg-gray-400 border-gray-500 rounded-md hover:bg-white  hover:text-black  px-2">
+                      Month
+                    </button>
+                  </div>
                 </div>
-                <div className="">
-                
-                  <button className="mx-4 text-white bg-gray-400 border-gray-500 rounded-md hover:bg-white  hover:text-black px-2">Day</button>
-                  <button className="mx-4 text-white bg-gray-400 border-gray-500 rounded-md hover:bg-white  hover:text-black  px-2">Week</button>
-                  <button className="mx-4 text-white bg-gray-400 border-gray-500 rounded-md hover:bg-white  hover:text-black  px-2">Month</button>
-                </div>
+                <hr className="h-px my-4 bg-gray-400 border-0  w-full" />
               </div>
-              <hr className="h-px my-4 bg-gray-400 border-0  w-full" />
 
-
-            
-            </div>
-          
-
-            </div>
-            <iframe
+              <div className="">
+              <iframe
   style={{
-    background: "#FFFFFF",
+    background: "transparent",
     border: "none",
     borderRadius: 2,
-    boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)"
+    boxShadow: "0 2px 10px 0 rgba(70, 76, 79, .2)",
+    width: "50vw",
+    height: "70vh"
   }}
-  width={640}
-  height={480}
-  src="https://charts.mongodb.com/charts-project-0-ggenymo/embed/charts?id=6641b037-07cf-4f3d-81ec-a463c126b630&maxDataAge=3600&theme=light&autoRefresh=true"
+  src="https://charts.mongodb.com/charts-project-0-ggenymo/embed/dashboards?id=6641afb1-dc15-4e3c-8098-e161bf672dc8&theme=light&autoRefresh=true&maxDataAge=3600&showTitleAndDesc=false&scalingWidth=scale&scalingHeight=scale"
 />
+
+              </div>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
-    
   );
 };
 
